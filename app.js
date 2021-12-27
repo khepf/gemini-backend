cb58109da01545c262f158fd84e84f82e69edf0f
Jeremy Kepf <jeremy1975@gmail.com>
2021-12-27 18:56:24 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,42 @@
+const path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const postsRoutes = require("./routes/posts");
+const userRoutes = require("./routes/user");
+
+const app = express();
+
+mongoose
+  .connect(
+    "mongodb+srv://gemini:" +
+      process.env.MONGO_ATLAS_PW +
+      "@gcsandboxcluster.wiugq.mongodb.net/baseballcards?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("Connected to database!");
+  })
+  .catch((err) => {
+    console.log("Houston we have a problem! " + err);
+  });
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/images", express.static(__dirname + "/images"));
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, DELETE, PUT, OPTIONS"
+  );
+  next();
+});
+
+app.use("/api/posts", postsRoutes);
+app.use("/api/user", userRoutes);
+
+module.exports = app;
