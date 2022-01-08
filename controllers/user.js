3e86871eabe629b0ade582bcb0a399abe9df1a36
Jeremy Kepf <jeremy1975@gmail.com>
2022-01-08 12:13:15 -0500

    add user functionality

--- controllers/user.js
+++ controllers/user.js
@@ -69,3 +69,86 @@ exports.userLogin = (req, res, next) => {
       });
     });
 };
+
+exports.getUsers = (req, res, next) => {
+  const pageSize = +req.query.pagesize;
+  const currentPage = +req.query.page;
+  const userQuery = User.find();
+  let fetchedUsers;
+  if (pageSize && currentPage) {
+    userQuery.skip(pageSize * (currentPage - 1)).limit(pageSize);
+  }
+  userQuery
+    .then((documents) => {
+      fetchedUsers = documents;
+      return User.count();
+    })
+    .then((count) => {
+      res.status(200).json({
+        message: "Users fetched successfully!",
+        users: fetchedUsers,
+        maxUsers: count,
+      });
+    })
+    .catch((error) => {
+      res.status(500).json({
+        message: "Fetching users failed!",
+      });
+    });
+};
+
+exports.getUser = (req, res, next) => {
+  User.findById(req.params.id)
+    .then((user) => {
+      if (user) {
+        res.status(200).json(user);
+      } else {
+        res.status(404).json({ message: "User not found!" });
+      }
+    })
+    .catch((error) => {
+      res.status(500).json({
+        message: "Fetching user failed!",
+      });
+    });
+};
+
+exports.updateUser = (req, res, next) => {
+  const user = new User({
+    _id: req.body.id,
+    username: req.body.username,
+    email: req.body.email,
+    role: req.body.role,
+  });
+  User.updateOne({ _id: req.params.id }, user)
+    .then((result) => {
+      if (result.modifiedCount > 0) {
+        res.status(200).json({ message: "User Update successful!" });
+      } else {
+        res.status(401).json({ message: "Not authorized!" });
+      }
+    })
+    .catch((error) => {
+      res.status(500).json({
+        message: "Couldn't update user!",
+        error: "error",
+      });
+    });
+};
+
+exports.deleteUser = (req, res, next) => {
+  User.deleteOne({ _id: req.params.id })
+    .then((result) => {
+      console.log(result);
+      if (result.deletedCount > 0) {
+        res.status(200).json({ message: "Deletion successful!" });
+      } else {
+        res.status(401).json({ message: "Not authorized!" });
+      }
+    })
+    .catch((error) => {
+      res.status(500).json({
+        message: "Deleting posts failed!",
+      });
+    });
+};
--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,20 @@
 const express = require("express");
 
 const UserController = require("../controllers/user");
+const checkAuth = require("../middleware/check-auth");
 
 const router = express.Router();
 
 router.post("/signup", UserController.createUser);
 
 router.post("/login", UserController.userLogin);
+
+router.put("/:id", checkAuth, UserController.updateUser);
+
+router.get("", UserController.getUsers);
+
+router.get("/:id", UserController.getUser);
+
+router.delete("/:id", checkAuth, UserController.deleteUser);
 
 module.exports = router;
